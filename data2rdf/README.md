50b1cad0be5966ead4f921db72117a33755fc026
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2017-02-20 13:41:40 +0100

    testing and readme

--- data2rdf/README.md
+++ data2rdf/README.md
@@ -1,5 +1,15 @@
-curl -H "Content-Type: application/json" -X POST -d '{"input":"xyz","type":"xyz"}' http://localhost:8080/disambiguation
+#Data2RDF Wrapper#
 
-curl -H "Content-Type: application/json" -X POST -d '{ "input": "The philosopher and mathematician Leibniz was born in Leipzig in 1646.", "namedEntities": { "output": [ { "namedEntity": "Leibniz", "start": 35, "end": 42, "offset": 7, "disambiguatedURL": "http://dbpedia.org/resource/Gottfried_Wilhelm_Leibniz" }, { "namedEntity": "Leipzig", "start": 55, "end": 62, "offset": 7, "disambiguatedURL": "http://dbpedia.org/resource/Leipzig" } ] } }' http://localhost:8080/text2k
+This is a wrapper project for the QAMEL server-side implementations. See also deliverable D2.1.
 
-curl -H "Content-Type: application/json" -X POST -d '{"input":"xyz","type":"xyz"}' http://localhost:8080/recognition
+You can run this project using `mvn clean test spring-boot:run`
+
+###Testing interface###
+We provide three different interfaces
+
+`curl -H "Content-Type: application/json" -X POST -d '{"input": "The philosopher and mathematician Leibniz was born in Leipzig in 1646.","type":"xyz"}' http://localhost:8080/recognition`
+
+`curl -H "Content-Type: application/json" -X POST -d '{"input": "The philosopher and mathematician <entity>Leibniz</entity> was born in <entity>Leipzig</entity> in 1646.","type":"xyz"}' http://localhost:8080/disambiguation`
+
+
+`curl -H "Content-Type: application/json" -X POST -d '{ "input": "The philosopher and mathematician Leibniz was born in Leipzig in 1646.", "namedEntities": { "output": [ { "namedEntity": "Leibniz", "start": 35, "end": 42, "offset": 7, "disambiguatedURL": "http://dbpedia.org/resource/Gottfried_Wilhelm_Leibniz" }, { "namedEntity": "Leipzig", "start": 55, "end": 62, "offset": 7, "disambiguatedURL": "http://dbpedia.org/resource/Leipzig" } ] } }' http://localhost:8080/text2k`
---
+++ data2rdf/src/test/java/org/aksw/qamel/data2rdf/ControllerTest.java
@@ -0,0 +1,64 @@
+package org.aksw.qamel.data2rdf;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { Application.class })
+@WebAppConfiguration
+public class ControllerTest {
+
+	@Autowired
+	private WebApplicationContext ctx;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+		                              .build();
+	}
+//TODO write tests for the other controller endpoints once the API is stable
+	@Test
+	public void disambiguation() throws Exception {
+
+		this.mockMvc.perform(post("/disambiguation").content("{\"input\": \"The philosopher and mathematician Leibniz was born in Leipzig in 1646.\",\"type\": \"text\"}")
+		                                            .contentType(MediaType.APPLICATION_JSON))
+		            .andDo(print())
+		            .andExpect(status().isOk())
+		            .andExpect(content().json("{"
+		            		+ "			\"output\": ["
+		            		+ "				{"
+		            		+ "					\"namedEntity\": \"Leibniz\","
+		            		+ "					\"start\": 35,"
+		            		+ "					\"end\": 42,"
+		            		+ "					\"offset\": 7,"
+		            		+ "					\"disambiguatedURL\": \"http://dbpedia.org/resource/Gottfried_Wilhelm_Leibniz\""
+		            		+ "				},"
+		            		+ "				{"
+		            		+ "					\"namedEntity\": \"Leipzig\","
+		            		+ "					\"start\": 55,"
+		            		+ "					\"end\": 62,"
+		            		+ "					\"offset\": 7,"
+		            		+ "					\"disambiguatedURL\": \"http://dbpedia.org/resource/Leipzig\""
+		            		+ "				}"
+		            		+ "			]"
+		            		+ "		}"));
+		
+		
+	}
+}
