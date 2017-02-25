a21f8efdc389866352533fb75d866cf3db39df43
micha <michoffmann.potsdam@gmail.com>
2017-02-26 00:00:46 +0100

    fixed downloadscript

--- queryAlternatives/loadw2v-vectors.sh
+++ queryAlternatives/loadw2v-vectors.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget http://www-nlp.stanford.edu/data/wordvecs/glove.6B.zip -O src/main/resources/glove.6B.zip
-unzip src/main/resources/glove.6B.zip
+unzip src/main/resources/glove.6B.zip -d src/main/resources/glove.6B
